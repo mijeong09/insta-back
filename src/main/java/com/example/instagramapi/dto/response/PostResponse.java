3c3704c2b4b129eec3e05f10552012e8c05cf316
mijeong09 <me9809180@gmail.com>
2025-12-10 13:35:38 +0900

    PostResponse user => author 변경

--- src/main/java/com/example/instagramapi/dto/response/PostResponse.java
+++ src/main/java/com/example/instagramapi/dto/response/PostResponse.java
@@ -15,7 +15,7 @@ public class PostResponse {
     private String imageUrl;
     private LocalDateTime createdAt;
 
-    private UserResponse user;
+    private UserResponse author;
 
     private boolean liked;
     private long likeCount;
@@ -26,7 +26,7 @@ public class PostResponse {
                 .id(post.getId())
                 .content(post.getContent())
                 .imageUrl(post.getImageUrl())
-                .user(UserResponse.from(post.getUser()))
+                .author(UserResponse.from(post.getUser()))
                 .createdAt(post.getCreatedAt())
                 .liked(false)
                 .likeCount(0)
@@ -44,7 +44,7 @@ public class PostResponse {
                 .id(post.getId())
                 .content(post.getContent())
                 .imageUrl(post.getImageUrl())
-                .user(UserResponse.from(post.getUser()))
+                .author(UserResponse.from(post.getUser()))
                 .createdAt(post.getCreatedAt())
                 .liked(liked)
                 .likeCount(likeCount)
